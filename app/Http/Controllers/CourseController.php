a75df98d732bbeaf6e117353ba1acafc48c3fa04
Scrivs <scrivs@gmail.com>
2018-08-07 09:40:52 -0700

    init

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -16,7 +16,7 @@ class CourseController extends Controller
     public function index()
     {
         
-        $courses = Course::get();
+        $courses = Course::where('status', 1)->get();
 
         return view('course.index', compact('courses'));
     }
