26b56f4371e586250a9352a04b60736cd34ada7f
Scrivs <scrivs@gmail.com>
2018-08-17 15:48:50 -0700

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,4 +10,25 @@
     </div>
   </div>
 </section>
+<section class="content lesson">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<p>On a site like this I'm supposed to sell you some type of dream.</p>
+				<p>You see, to succeed in online business you have to make the potential buyer believe that you are going to be the one to help them get from Point A to Point B.</p>
+				<p>In essence, you're going to provide them with the bridge that will close THE GAP.</p>
+				<p>In the <em>how to make money</em> industry this especially holds true but there is a big problem. Simply learning the techniques is often not enough because you'll find a way to prevent your own success.</p>
+				<p><strong>You yourself have to be brave (or stupid) enough to conquer many different things.</strong></p>
+				<p>(Now you see where the name comes from.)</p>
+				<p>So, am I the one that is going to help you out with all of this? I'll certainly try but you have to be open to the idea of pushing yourself.</p>
+				<p>Wait, you're probably trying to figure out who in the hell I am. Sorry about that.</p>
+				<p>My name is Paul Scrivens and I'm fortunate enough to run three 6-figure (this means they make 6-figures a year) business and three 5-figure businesses.</p>
+				<p>It allows me to pretty much live the life that I want.</p>
+				<p>That means hanging out with family and friends whenever I want. Not worrying about how much we can spend at the grocery store.</p>
+				<p><strong>Most importantly I get to focus on being the best version of me.</strong></p>
+				<p>It is awesome and I know others want to have the same type of experience so here I am teaching what I know.</p>
+			</div>
+		</div>
+	</div>
+</section>
 @endsection
