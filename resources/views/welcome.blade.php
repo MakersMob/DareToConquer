9df8fb04498fdf62132930b185dc10623a382d27
Scrivs <scrivs@gmail.com>
2018-10-02 23:18:47 -0700

    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,9 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-12">
+				<div>
+					<script src="https://fast.wistia.com/embed/medias/qoxseo2270.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_qoxseo2270 videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/qoxseo2270/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>
+				</div>
 				<p>On a site like this I'm supposed to sell you some type of dream.</p>
 				<p>You see, to succeed in online business you have to make the potential buyer believe that you are going to be the one to help them get from Point A to Point B.</p>
 				<p>In essence, you're going to provide them with the bridge that will close THE GAP.</p>
