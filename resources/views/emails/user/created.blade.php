7c796c3e1ce29edde6dedc7f1f358ed9353e0114
Scrivs <scrivs@gmail.com>
2018-10-04 10:34:00 -0700

    init

--- resources/views/emails/user/created.blade.php
+++ resources/views/emails/user/created.blade.php
@@ -2,6 +2,8 @@
 
 <p>Just writing to let you know that your account on Dare to Conquer has been created. It's this email address and your password is 'daretoconquer'.</p>
 
+<p>You can change this password after logging in.</p>
+
 <p>You can log in by <a href="https://daretoconquer.com/login">clicking this link</a>.</p>
 
 <p>- Scrivs</p>
