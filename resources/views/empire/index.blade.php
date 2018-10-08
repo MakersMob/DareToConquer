f9214cbaf2bbb027980c56f7fcaa91502d79a7c3
Scrivs <scrivs@gmail.com>
2018-10-08 09:41:56 -0700

    init

--- resources/views/empire/index.blade.php
+++ resources/views/empire/index.blade.php
@@ -19,25 +19,31 @@
 					<iframe width="1280" height="720" src="https://www.youtube.com/embed/WN-klUC1USw" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>
 				</div>
 			</div>
-			<div class="col-12 col-lg-6" style="margin-top: 2rem;">
-				<h2>Never Miss an Update</h2>
+		</div>
+	</div>
+</section>
+<section class="content">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-12 col-lg-6">
+				<h2 style="margin-top: 0;"><strong>Never Miss an Empire Builder Update</strong></h2>
 				<div class="card">
 					<div class="card-body">
 						<form action="https://www.getdrip.com/forms/143053519/submissions" method="post" data-drip-embedded-form="143053519">
 						    <div class="form-group">
-						        <label class="form-label" for="drip-email">Email Address</label><br />
+						        <label class="form-label" for="drip-email">Gimme Your Email Address...Gimme, Gimme</label><br />
 						        <input class="form-control" type="email" id="drip-email" name="fields[email]" value="" />
 						    </div>
 						    <div class="form-check">
 						        <input type="hidden" name="fields[eu_consent]" id="drip-eu-consent-denied" value="denied" />
 						        <input class="form-check-input" type="checkbox" name="fields[eu_consent]" id="drip-eu-consent" value="granted" />
-						        <label class="form-check-label" for="drip-eu-consent">Just to ensure this is you and follow EU GDPR guidelines, by checking this box you consent to me sending you more kick ass emails than those other people with their buy buy buy type of stuff.</label>
+						        <label class="form-check-label" for="drip-eu-consent">By checking this box you understand that I am awesome and I only send awesome emails.</label>
 						    </div>
 						    <div>
 						        <input type="hidden" name="fields[eu_consent_message]" value="Just to ensure this is you and follow EU GDPR guidelines, by checking this box you consent to me sending you more kick ass emails than others send.">
 						    </div>
-						  <div>
-						    <button type="submit" class="btn btn-lg btn-primary btn-block">Receive All Empire Builder Updates</button>
+						  <div style="margin-top: 1rem;">
+						    <button type="submit" class="btn btn-primary btn-block">Receive All Empire Builder Updates</button>
 						  </div>
 						</form>
 					</div>
@@ -46,7 +52,7 @@
 		</div>
 	</div>
 </section>
-<section class="content">
+<section class="content smoke">
 	<div class="container">
 		<div class="row justify-content-center">
 			<div class="col-12">
