746c4a4f2755347d404bef6a215972c51e47efcf
Scrivs <scrivs@gmail.com>
2018-10-10 01:13:56 -0700

    init

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -5,7 +5,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-12">
-				<h1 class="">Online Business FAQ</h1>
+				<h1 class="billboard"><strong>Online Business FAQ</strong></h1>
 				@role('admin')
 					<div class="" style="margin-top: 2rem;"><a href="/questions/create" class="btn btn-primary">Add Question</a></div>
 				@endrole
