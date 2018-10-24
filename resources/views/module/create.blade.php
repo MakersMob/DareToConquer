bf825015def5ef4081501ab1356e4d3647c6f3dc
Scrivs <scrivs@gmail.com>
2018-10-23 22:51:31 -0700

    init

--- resources/views/module/create.blade.php
+++ resources/views/module/create.blade.php
@@ -12,7 +12,7 @@
 </section>
 <section class="content smoke">
 	<div class="container">
-		<div class="row">
+		<div class="row justify-content-center">
 			<div class="col-12 col-lg-6">
 				<div class="card">
 					<div class="card-body">
