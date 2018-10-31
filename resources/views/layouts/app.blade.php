4b9d2221d4fdfa03c2ae1c850c2c67ba8fa8ee5b
Scrivs <scrivs@gmail.com>
2018-10-30 20:42:48 -0700

    init

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,6 +75,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#">{{ Auth::user()->points }} Points</a>
                                     <a class="dropdown-item" href="/member/edit">Edit Profile</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
