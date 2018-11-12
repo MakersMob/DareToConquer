640e95e47b6443159d6407315b26e79bc096cee7
Scrivs <scrivs@gmail.com>
2018-11-11 21:31:30 -0800

    init

--- resources/views/journey/show.blade.php
+++ resources/views/journey/show.blade.php
@@ -18,7 +18,7 @@
   <div class="container">
     <div class="row">
       <div class="col-12 col-lg-6">
-        {{ $journey->description }}
+        {!! $journey->description !!}
       </div>
       <div class="col-12 col-lg-6">
         <table class="table">
