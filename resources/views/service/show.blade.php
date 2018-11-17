00a00f7933796633b5be9761aa049d92db4e44ad
Scrivs <scrivs@gmail.com>
2018-11-16 17:40:12 -0800

    init

--- resources/views/service/show.blade.php
+++ resources/views/service/show.blade.php
@@ -7,6 +7,7 @@
 			<div class="col-12">
 				<h2 class="preheader"><a href="/service">Member Services Directory</a></h2>
 				<h1 class="">{{ $service->name }}</h1>
+				<h3 class="subheader">by {{ $service->user->first_name }} {{ $service->user->last_name }}</h3>
 				@if(Auth::user()->id == $service->user_id)
 					<div class="" style="margin-top: 2rem;"><a href="/service/{{ $service->id }}/edit" class="btn btn-primary">Edit Service</a> <a href="/service/{{ $service->id }}/delete" class="btn btn-primary btn-alert">Delete Service</a></div>
 				@endrole
