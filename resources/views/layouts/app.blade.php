03e6b5277560e65ca9e5261a80acb6ac092223d3
Scrivs <scrivs@gmail.com>
2018-11-16 17:45:20 -0800

    init

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,6 +77,9 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="/exchange">Member Exchange</a>
                                 </div>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="/service">Member Services</a>
+                                </div>
                             </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
