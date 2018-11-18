4f3a268cc5031bd6be33cbfc73874b79941a5632
Scrivs <scrivs@gmail.com>
2018-11-18 07:01:14 -0800

    ini

--- app/Http/Controllers/ExchangeNicheController.php
+++ app/Http/Controllers/ExchangeNicheController.php
@@ -13,7 +13,7 @@ class ExchangeNicheController extends Controller
     	$niche = Niche::where('slug', $niche)->first();
     	$niches = Niche::orderBy('name', 'ASC')->get();
 
-    	$exchanges = Exchange::where('niche_id', $niche->id)->paginate(20);
+    	$exchanges = Exchange::where('status', 1)->where('niche_id', $niche->id)->paginate(20);
 
     	return view('exchange.niche.show', compact('exchanges', 'niches', 'niche'));
     }
