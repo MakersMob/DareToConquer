a4049452d2c951ce71ad2791a77f859c8ea7a0fb
Scrivs <scrivs@gmail.com>
2019-02-03 06:14:53 -0800

    init

--- resources/views/newsletter/index.blade.php
+++ resources/views/newsletter/index.blade.php
@@ -13,7 +13,11 @@
 <section class="content directory smoke">
 	<div class="container">
 		<div class="row">
-			<div class="col-12">
+			<div class="col-12 col-lg-6">
+				<h2>The Ish</h2>
+				<p>Every Sunday, the all-powerful Marybeth drops the Community Newsletter. Each week she saves a copy. Consider it your personal library without the clutter.</p>
+			</div>
+			<div class="col-12 col-lg-6">
 				<ul>
 					@foreach($newsletters as $newsletter)
 						<li><a href="/newsletter/{{$newsletter->id}}">{{ $newsletter->subject}}</a></li>
