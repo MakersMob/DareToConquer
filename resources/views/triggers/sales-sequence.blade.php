39e24fce875c05f6aa9934fb966fb6bd764f9d9e
Scrivs <scrivs@gmail.com>
2019-04-08 06:03:58 -0700

    init

---
+++ resources/views/triggers/sales-sequence.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app', ['title' => 'Creating a sales sequence that converts'])
+
+@section('content')
+<section class="welcome home">
+  <div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="">Sales Sequences and Products</h1>
+        </div>
+    </div>
+  </div>
+</section>
+<section class="content lesson">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<p>There is nothing fancy on this page, but I needed a page to send you to so I know you're interested in the new series of emails that I'll send out.</p>
+				<p>See you on Friday!</p>
+			</div>
+		</div>
+	</div>
+</section>
+@endsection
