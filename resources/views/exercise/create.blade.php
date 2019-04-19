d638f86caa95418a6ac26d2e1f33902db18a8163
Scrivs <scrivs@gmail.com>
2019-04-19 12:10:44 -0700

    init

--- resources/views/exercise/create.blade.php
+++ resources/views/exercise/create.blade.php
@@ -10,7 +10,7 @@
 		</div>
 	</div>
 </section>
-<section class="content smoke">
+<section class="content">
 	<div class="container">
 		<div class="row justify-content-center">
 			<div class="col-12 col-lg-8">
