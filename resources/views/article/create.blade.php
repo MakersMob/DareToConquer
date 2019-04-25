c55057b88791707f969b4250b5b22067a66422bc
Scrivs <scrivs@gmail.com>
2019-04-25 09:02:08 -0700

    init

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -10,32 +10,38 @@
 		</div>
 	</div>
 </section>
-<section class="content smoke">
+<section class="content">
 	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-12 col-lg-6">
+		<div class="row">
+			<div class="col-12">
 				<div class="card">
 					<div class="card-body">
 						{!! Form::open(['url' => 'article', 'enctype' => 'multipart/form-data']) !!}
-							<div class="form-group">
-								<label for="slug">Slug</label>
-								<input class="form-control" type="text" name="slug" id="slug">
-							</div>
-							<div class="form-group">
-								<label for="summary">Summary</label>
-								<input class="form-control" type="text" name="summary" id="summary">
-							</div>
-							<div class="form-group">
-								<label for="title">Title</label>
-								<input class="form-control" type="text" name="title" id="title">
-							</div>
-							<div class="form-group">
-								<label for="title">SEO Title</label>
-								<input class="form-control" type="text" name="seo_title" id="seo_title">
-							</div>
-							<div class="form-group">
-								<label for="content">Content</label>
-								<textarea class="form-control" name="content" rows="8"></textarea>
+							<div class="row">
+								<div class="col-12 col-lg-6">
+									<div class="form-group">
+										<label for="slug">Slug</label>
+										<input class="form-control" type="text" name="slug" id="slug">
+									</div>
+									<div class="form-group">
+										<label for="summary">Summary</label>
+										<input class="form-control" type="text" name="summary" id="summary">
+									</div>
+									<div class="form-group">
+										<label for="title">Title</label>
+										<input class="form-control" type="text" name="title" id="title">
+									</div>
+									<div class="form-group">
+										<label for="title">SEO Title</label>
+										<input class="form-control" type="text" name="seo_title" id="seo_title">
+									</div>
+								</div>
+								<div class="col-12 col-lg-6">
+									<div class="form-group">
+										<label for="content">Content</label>
+										<textarea class="form-control" name="content" rows="20"></textarea>
+									</div>
+								</div>
 							</div>
 							<div class="form-group">
 								<div class="custom-file">
