b67c7a6cfb057e3a161d28fc5592e41a6933fd59
Scrivs <scrivs@gmail.com>
2019-04-25 10:39:50 -0700

    init

--- resources/views/stop/show.blade.php
+++ resources/views/stop/show.blade.php
@@ -5,7 +5,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-12">
-				<h2 class="preheader"><a href="/journey/{{ $stop->journey->slug }}">{{ $stop->journey->title}}</a></h2>
+				<h3 class="preheader"><a href="/journey/{{ $stop->journey->slug }}">{{ $stop->journey->title}}</a></h3>
 				<h1 class="billboard"><strong>{{ $stop->name }}</strong></h1>
 				@role('admin')
 					<div class="" style="margin-top: 2rem;"><a href="/stop/{{ $stop->id }}/edit" class="btn btn-primary">Edit Stop</a></div>
