59850f7e7b7d2b8c3cdbd04592707f25568b4a70
Scrivs <scrivs@gmail.com>
2019-04-29 09:20:51 -0700

    init

--- resources/views/roadmap/index.blade.php
+++ resources/views/roadmap/index.blade.php
@@ -19,7 +19,7 @@
 					<div class="card-body">
 						<p>It's really hard for me to say that you should do the courses in a specific order because what you'll find is that there is a lot of crossover between them.</p>
 						<p>It might help you to go through SEO while also looking at half of the Pinterest course and keeping the Content Creation course in mind.</p>
-						<p>So what I've done is provided you with the natural order of courses to go through if you want to go through a full course before moving on and then also the wiggly route guide which will have you bouncing around different courses as you pick up pieces of knowledge.</p>
+						<p>So what I've done is provided you with the natural order of courses to go through if there is a very specific goal that you want to achieve.</p>
 						<p><strong>You should 100% go through all of the courses AT LEAST 2-3x if not more. There will be times where you forget certain things, missing certain things, or things just won't click until all of the pieces are in place.</strong></p> 
 					</div>
 				</div>
