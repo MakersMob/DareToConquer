80fecbfd5d3d0e56c2bf42f9116e234c155f0134
Scrivs <scrivs@gmail.com>
2019-04-30 08:21:19 -0700

    init

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -35,7 +35,7 @@ class PaymentController extends Controller
                 $description = $course->name;
                 break;
             case 'membership':
-                $amount = 597;
+                $amount = env('MEMBERSHIP_PRICE');
                 $id = 999;
                 $type = 'membership';
                 $description = 'DTC Membership';
